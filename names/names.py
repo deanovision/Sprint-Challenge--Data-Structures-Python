3d1a27e02c7b2bbf54cf8b148c6ee8188932fecd
deanovision <rodeanfraser@gmail.com>
2019-07-26 12:04:22 -0400

    finished mvp

--- names/names.py
+++ names/names.py
@@ -1,3 +1,4 @@
+
 import time
 
 start_time = time.time()
@@ -10,13 +11,21 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+cache = {}
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+for name in names_1:
+    cache[name] = name
+for name in names_2:
+    if name in cache:
+        duplicates.append(name)
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,18 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        if self.current == len(self.storage):
+            self.current = 0
+        self.storage[self.current] = item
+        self.current += 1
 
-  def get(self):
-    pass
+    def get(self):
+        temp = []
+        for i in range(0, len(self.storage)):
+            if self.storage[i] != None:
+                temp.append(self.storage[i])
+        return temp
